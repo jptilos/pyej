b4323d817953ad27b5d96a0c39f718252904a0d9
pyej <james.paulo.tilos@gmail.com>
2021-05-23 06:37:49 +0800

    Version 1 by James Paulo B.Tilos

---
+++ htmlreview.php
@@ -0,0 +1,41 @@
+<html>
+<title> Gene </title>
+<body>
+<?php include("index.php");
+?>
+	<center> HTML Review </center> <br>
+	<hr align="center" width="80%"> <br> <!--Underline-->
+	
+	<font color="blue" size="14" face="Times"> I am colorful.</font> <br> <br>
+	<!--Arial, Times, Verdana, Serifs, Courier New-->
+
+	<b><u><i> Good day </i> </u> </b> <br>
+
+<h1> Moguri </h1> <br>
+<h2> Minguin </h2> <br>
+<h3> Dubu </h3> <br>
+<h4> Shiba </h4> <br>
+<h5> chewweeeee </h5> <br>
+
+<table align="center" width="70" border="1">
+<tr>
+	<th> column1 </th> <!--th is title header -->
+	<th> column2 </th>
+	<th> column3 </th>
+</tr>
+
+<tr>
+	<td> 1 </td> <!--td is title division -->
+	<td> 2 </td>
+	<td> 3 </td>
+</tr>
+
+<tr>
+	<td> 4 </td>
+	<td> 5 </td>
+	<td> 6 </td>
+</tr>
+
+</table>
+</body> 
+</html>
